07b8b53e2a3317ea3f67cd540804fbd9e8716f60
jrkinouani <kinouani@gmail.com>
2018-08-13 11:08:11 +0400

    update branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180731081618) do
+ActiveRecord::Schema.define(version: 20180810055219) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "deliverymen", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +44,22 @@ ActiveRecord::Schema.define(version: 20180731081618) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_deliverymen_on_email", unique: true
     t.index ["reset_password_token"], name: "index_deliverymen_on_reset_password_token", unique: true
+  end
+
+  create_table "firstlivreurs", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "phone"
+    t.string "delivery_days"
+    t.string "city"
+    t.string "transport"
+    t.string "usual_route"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "citystart"
+    t.string "city_depart"
+    t.string "city_arrival"
   end
 
   create_table "posts", force: :cascade do |t|
