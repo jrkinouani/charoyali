788ebbc5aeb242f429b083507deb2d8c326c56ce
jrkinouani <kinouani@gmail.com>
2018-08-13 09:19:49 +0400

    hotfix root heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   get '/firstlivreurs/new' => "firstlivreurs#new"
   get '/firstlivreurs/thanks' => "firstlivreurs#thanks"
 
-
+  root :to => "firstlivreurs#new"
 resources :firstlivreurs
   resources :posts
-  root :to => "firstlivreurs#new"
+  
   get 'static_pages/ventes' => "static_pages#ventes"
   get 'static_pages/accueil'
   get 'static_pages/conditions'
